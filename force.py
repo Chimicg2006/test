8d39853c3153855431077707b9c3365612614402
unknown <02230060.cst@rub.edu.bt>
2024-02-28 19:01:08 +0600

    Adding force.py file

---
+++ force.py
@@ -0,0 +1,26 @@
+print("Newton's Second Law of Motion")
+print("-------------------------------------------")
+
+print("Select the missing value:")
+print("1. Mass (m)")
+print("2. Acceleration (a)")
+print("3. Force (f)")
+missing_value_choice = input("Enter the option number for the missing value: ")
+
+if missing_value_choice == "1":
+    a = float(input("Enter acceleration (a): "))
+    f = float(input("Enter force (f): "))
+    m = f / a 
+    print(f"Mass (m) = {m}")
+elif missing_value_choice == "2":
+    m = float(input("Enter mass (m): "))
+    f = float(input("Enter force (f): "))
+    a = f / m 
+    print(f"Acceleration (a) = {a}")
+elif missing_value_choice == "3":
+    m = float(input("Enter mass (m): "))
+    a = float(input("Enter  acceleration (a): "))
+    f = m * a 
+    print(f"Force (f) = {f}")
+else:
+    print("Invalid option selected for the missing value.")
